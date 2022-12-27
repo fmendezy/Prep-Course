0bd1bf5eee63b340657aa8da851d746616962a50
Francisco Mendezy <fmy@outlook.cl>
2022-12-27 09:41:18 -0300

    Avanzando en el examen

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -132,18 +132,17 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-
-   if(num1 > 0 && num1 > num2 && num1 > num3){
-   return "Número 1 es mayor y positivo";
-   } else if(num1 < 0 || num2 < 0 || num3 < 0){
-     return "Hay negativos";
-   } else if(num3 > num1 && num3 > num2){
-     return num3+1;
-   } else if(num1 == 0 || num2 == 0 || num3 == 0){
-     return "Error";
-   } else {
-     return false;
-   }
+   if(num1 < 0 || num2 < 0 || num3 < 0){
+    return "Hay negativos";
+  } else if(num1 === 0 || num2 === 0 || num3 === 0){
+    return "Error";
+  } else if(num1 > num2 && num1 > num3 && num1 > 0){
+    return "Número 1 es mayor y positivo";
+  } else if(num3 > num1 && num3 > num2){
+    return num3+1;
+  } else {
+    return false;
+  }
 
   // Opcion 2 de desarrollo
   // if(num1 < 0 || num2 < 0 || num3 < 0){
@@ -165,7 +164,13 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+	if (numero == 0 || numero == 1 || numero == 4) return false;
+	for (let i = 2; i < numero / 2; i++) {
+		if (numero % i == 0) return false;
+	}
+	return true;
 }
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
